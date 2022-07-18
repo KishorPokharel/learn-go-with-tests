11c505a8a975c2559056706c93cd5445477dff96
KishorPokharel <kishorpokharel7@gmail.com>
2022-07-18 21:11:52 +0545

    working with pointers and errors, creating new types from existing ones and more testing

---
+++ pointers_and_errors/wallet_test.go
@@ -0,0 +1,52 @@
+package main
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		want := Bitcoin(10)
+		assertBalance(t, wallet, want)
+	})
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: 20}
+		err := wallet.Withdraw(Bitcoin(10))
+		want := Bitcoin(10)
+		assertNoError(t, err)
+		assertBalance(t, wallet, want)
+	})
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{balance: startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+		want := Bitcoin(20)
+		assertError(t, err, ErrInsufficientFunds)
+		assertBalance(t, wallet, want)
+	})
+}
+
+func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
+	t.Helper()
+	got := wallet.Balance()
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("wanted an error but did not get one")
+	}
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func assertNoError(t testing.TB, err error) {
+	t.Helper()
+	if err != nil {
+		t.Errorf("got an error but did not want one")
+	}
+}
